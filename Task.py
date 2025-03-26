2790e850cae6c0325a1c51aa95c7206f5513dfe3
unknown <239200@TECH.WYB.AC.LK>
2025-03-26 19:40:45 +0530

    first commit

---
+++ Task.py
@@ -0,0 +1,5 @@
+num=int(input("Enter your number:-"))
+print (num)
+
+name=str(input("Enter your name:-"))
+print (name)
---
+++ number.py
@@ -0,0 +1,8 @@
+num1=int(input("Enter no 1:-"))
+print(num1)
+num2=int(input("Enter no 1:-"))
+print(num2)
+
+
+number = num1+num2
+print("number",number)
